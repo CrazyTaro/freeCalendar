93623f670ff11264ac75b00fcdb3e3bc175a9bde
Grant <522852537@qq.com>
2017-07-16 23:42:49 +0800

    修复日期工具类可能导致崩溃的问题

--- calendarlib/src/main/java/com/taro/calendar/lib/utils/DateUtils.java
+++ calendarlib/src/main/java/com/taro/calendar/lib/utils/DateUtils.java
@@ -5,24 +5,23 @@ package com.taro.calendar.lib.utils;
  */
 
 public class DateUtils {
-    private static int[] dayMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static int[] mDayMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     public static int getNumDayOfMonth(int year, int month) {
         if (month < 0 || month > 11) {
             return 0;
         }
-        int i = dayMonth[(month - 1)];
-        if ((isBigYear(year)) && (month == 2)) {
+        int i = mDayMonth[month];
+        if ((isLeapYear(year)) && (month == 1)) {
             i++;
         }
         return i;
     }
 
-    public static boolean isBigYear(int year) {
+    public static boolean isLeapYear(int year) {
         if (year % 400 == 0) {
             return true;
         }
         return (year % 4 == 0) && (year % 100 != 0);
     }
-
 }
